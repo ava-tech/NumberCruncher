2e8117265b806ff3f2d1f278198c4dadc77240b1
Kaiser Alam <kaiseralam@msn.com>
2016-09-03 10:09:14 -0700

    Added POST request for ITCuties

---
+++ NumberCruncher/src/com/api/POSTSenderExample.java
@@ -0,0 +1,53 @@
+package com.api;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class POSTSenderExample {
+	
+	public String echoCities(String query) throws IOException{
+		
+		String encodedQuery = URLEncoder.encode(query, "UTF-8");
+		String postData = "e=" + encodedQuery;
+		
+		URL url = new URL("http://echo.itcuties.com/");
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setDoOutput(true);
+		connection.setRequestMethod("POST");
+		connection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+		connection.setRequestProperty("Content-Length",  String.valueOf(postData.length()));
+		
+		OutputStream os = connection.getOutputStream();
+		os.write(postData.getBytes());
+		
+		
+		StringBuilder response = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		
+		String line;
+		
+		while((line = br.readLine()) != null){
+			response.append(line);
+		}
+		
+		br.close();
+		os.close();
+		
+		return response.toString();
+	}
+
+	public static void main(String[] args) {
+		try{
+			System.out.println(new POSTSenderExample().echoCities("Hi There"));
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+
+	}
+
+}
